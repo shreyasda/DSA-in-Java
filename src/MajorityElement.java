51dcf9048d1554f0e378d61498418bb7cd4336c7
shreyasda <shreyasshrivatsa@gmail.com>
2024-08-23 00:21:49 +0530

    169.Majority Element

---
+++ src/MajorityElement.java
@@ -0,0 +1,26 @@
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(count == 0){
+                candidate = nums[i];
+            }
+
+            if(nums[i] == candidate){
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
+
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{3,3,2,4,4,4};
+        System.out.println(majorityElement(nums));
+    }
+}
